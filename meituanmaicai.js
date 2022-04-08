dd168959e3b836415b4487d82ee1f6bfcb93317f
江佳俊 <465021744@qq.com>
2022-04-08 11:53:33 +0800

    添加了更多的错误情况

--- meituanmaicai.js
+++ meituanmaicai.js
@@ -1,6 +1,6 @@
 const appName = "美团买菜";
 launchApp(appName);
-sleep(10);
+sleep(500);
 
 auto.waitFor()
 // 点击结算
@@ -19,42 +19,67 @@ const clickGiveup= () => {
 const clickPay = () => {
 	className("android.widget.TextView").text("立即支付").findOne().parent().click()
 }
+//拥堵繁忙
+const clickBusy = () => {
+	className("android.view.View").depth(15).findOne().parent().click()
+}
+//约满
+const clickFull = () => {
+	className("android.widget.TextView").text("我知道了").findOne().parent().click()       
+}
 
 const hasText = (text) => {
 	return textStartsWith(text).exists() // 是否存在指定文本
 }
 
 const start = () => {
-	// 是否有结算按钮
-	if (hasText("结算")) 
-    {
-		// 点击结算
-		clickSettle()
-		sleep(10)
-		start()
-	} 
-    else if (hasText('当前不在可下单时段') || hasText('很抱歉')) 
-    {
-		clickKnow()
-		sleep(10)
-		start()
-	} 
-    else if (hasText('立即支付')) 
-    {
+	console.show()
+	if (hasText('立即支付'))
+	{
+		console.log('立即支付')
 		clickPay()
-		sleep(10)
+		sleep(500)
 		start()
-        toast('抢到了')
+		console.log('抢到了')
 	} 
 	else if (hasText('放弃机会')) 
-    {
+	{
+		console.log('放弃机会')
 		clickGiveup()
-		sleep(10)
+		sleep(500)
 		start()
 	} 
-    else 
-    {
-        sleep(10)
+	else if (hasText('结算人数')) 
+	{
+		console.log('前方拥堵')
+		clickBusy()
+		sleep(500)
+		start()
+	} 
+	else if (hasText('当前不在可下单时段') || hasText('很抱歉')) 
+	{
+		console.log('当前不在可下单时段')
+		clickKnow()
+		sleep(500)
+		start()
+	} 
+	else if (hasText('配送运力已约满') ) 
+	{
+		console.log('已约满')
+		clickFull()
+		sleep(500)
+		start()
+	} 
+	else if (hasText("结算"))
+	{
+		console.log("结算")
+		clickSettle()
+		sleep(500)
+		start()
+	} 
+	else 
+	{
+		sleep(500)
 		start()
 	}
 }
